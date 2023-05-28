d58131d367c8018f8234aff5fabeff48a036abe2
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 22:33:28 +0300

    Add routes for user pages

--- app.js
+++ app.js
@@ -53,6 +53,53 @@ router.get('/unauthorized/img/telegram.png',function(req,res){
 });
 
 
+router.get('/user/css/main.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/css/main.css'));
+});
+router.get('/user/img/logo.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/logo.png'));
+});
+router.get('/user/img/user-logo.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/user-logo.png'));
+});
+router.get('/user/img/phone.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/phone.png'));
+});
+router.get('/user/img/point-plus.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/point-plus.png'));
+});
+router.get('/user/img/time.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/time.png'));
+});
+router.get('/user/img/marker.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/marker.png'));
+});
+router.get('/user/img/vk.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/vk.png'));
+});
+router.get('/user/img/whatsapp.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/whatsapp.png'));
+});
+router.get('/user/img/ok.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/ok.png'));
+});
+router.get('/user/img/telegram.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/telegram.png'));
+});
+router.get('/user/img/user-photo.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/user-photo.png'));
+});
+router.get('/profile/img/logo.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/logo.png'));
+});
+router.get('/profile/img/user-photo.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/user-photo.png'));
+});
+router.get('/profile/img/phone.png',function(req,res){
+  res.sendFile(path.join(__dirname+'/img/phone.png'));
+});
+
+
 
 /* Routes for Unauthorized user */
 router.get('/unauthorized/home',function(req,res){
@@ -82,6 +129,48 @@ router.get('/unauthorized/css/unauthorized/about.css',function(req,res){
   res.sendFile(path.join(__dirname+'/css/unauthorized/about.css'));
 });
 
+
+
+/* Routes for User */
+router.get('/user/home',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/main-page.html'));
+});
+router.get('/user/appointments/create',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/appointments-create.html'));
+});
+router.get('/user/css/user/appointments-create.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/css/user/appointments-create.css'));
+});
+router.get('/user/doctors',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/doctors.html'));
+});
+router.get('/user/prices',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/prices.html'));
+});
+router.get('/user/clinic/info',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/about.html'));
+});
+router.get('/profile/1',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/profile.html'));
+});
+router.get('/profile/1/edit',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/profile-edit.html'));
+});
+router.get('/user/appointments',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/appointments.html'));
+});
+router.get('/user/payments',function(req,res){
+  res.sendFile(path.join(__dirname+'/user/payments.html'));
+});
+router.get('/user/css/user/about.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/css/user/about.css'));
+});
+router.get('/profile/css/user/profile-edit.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/css/user/profile-edit.css'));
+});
+router.get('/profile/css/main.css',function(req,res){
+  res.sendFile(path.join(__dirname+'/css/main.css'));
+});
 
 app.use('/', router);
 app.listen(process.env.port || 3000);
