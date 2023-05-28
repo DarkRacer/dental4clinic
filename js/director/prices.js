e1b11b776bbdf163a8e5815f2955b0034191e975
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 15:15:23 +0300

    Add and adaptive edit prices dialog for director

--- js/director/prices.js
+++ js/director/prices.js
@@ -5,3 +5,26 @@ document.querySelector('#openCreatePriceDialog').onclick = function() {
 document.querySelector('#createPriceDialogClose').onclick = function() {
   createPriceDialog.close();
 }
+document.querySelector('#createPriceDialogSave').onclick = function() {
+  createPriceDialog.close();
+}
+
+var pricesDialog = document.querySelector('#pricesDialog');
+document.querySelector('#openPricesDialog').onclick = function() {
+  pricesDialog.show();
+}
+document.querySelector('#pricesDialogClose').onclick = function() {
+  pricesDialog.close();
+}
+
+var editPriceDialog = document.querySelector('#editPriceDialog');
+document.querySelector('#openEditPriceDialog').onclick = function() {
+  pricesDialog.close();
+  editPriceDialog.show();
+}
+document.querySelector('#editPriceDialogClose').onclick = function() {
+  editPriceDialog.close();
+}
+document.querySelector('#editPriceDialogSave').onclick = function() {
+  editPriceDialog.close();
+}
