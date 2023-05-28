fbd5c8205bb5b7b0e7a0a0cbd4969fb17d0e1ee4
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 17:42:18 +0300

    Add and adaptive create admin dialog for director

--- js/director/admins.js
+++ js/director/admins.js
@@ -8,3 +8,13 @@ document.querySelector('#deleteDialogClose').onclick = function() {
 document.querySelector('#backButtonDeleteDialogClose').onclick = function() {
   deleteDialog.close();
 }
+var createAdminDialog = document.querySelector('#createAdminDialog');
+document.querySelector('#openCreateAdminDialog').onclick = function() {
+  createAdminDialog.show();
+}
+document.querySelector('#createAdminDialogClose').onclick = function() {
+  createAdminDialog.close();
+}
+document.querySelector('#createAdminDialogSave').onclick = function() {
+  createAdminDialog.close();
+}
