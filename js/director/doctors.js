e4e2df667b9079b3d39599b7c72c8aa87e69904e
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 16:19:49 +0300

    Add and adaptive dialog for add doctor for director

--- js/director/doctors.js
+++ js/director/doctors.js
@@ -5,3 +5,15 @@ document.querySelector('#openDoctorsDialog').onclick = function() {
 document.querySelector('#doctorsDialogClose').onclick = function() {
   doctorsDialog.close();
 }
+
+var createDoctorDialog = document.querySelector('#createDoctorDialog');
+document.querySelector('#openCreateDoctorDialog').onclick = function() {
+  createDoctorDialog.show();
+}
+document.querySelector('#createDoctorDialogClose').onclick = function() {
+  createDoctorDialog.close();
+}
+
+document.querySelector('#createDoctorDialogSave').onclick = function() {
+  createDoctorDialog.close();
+}
