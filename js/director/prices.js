6cf000614125b560bc94132fe84445460008a4e3
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 15:01:29 +0300

    Add and adaptive create prices dialog for director

---
+++ js/director/prices.js
@@ -0,0 +1,7 @@
+var createPriceDialog = document.querySelector('#createPriceDialog');
+document.querySelector('#openCreatePriceDialog').onclick = function() {
+  createPriceDialog.show();
+}
+document.querySelector('#createPriceDialogClose').onclick = function() {
+  createPriceDialog.close();
+}
