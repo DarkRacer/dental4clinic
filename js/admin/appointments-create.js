8d1f8f17b64d3c26e015fbd2845850e43f693baf
Dark Racer <schtscherbakov@gmail.com>
2023-05-28 04:16:44 +0300

    Add and adaptive appointments create page for admin

---
+++ js/admin/appointments-create.js
@@ -0,0 +1,7 @@
+var requestsDialog = document.querySelector('#requestsDialog');
+document.querySelector('#openRequestsDialog').onclick = function() {
+  requestsDialog.show();
+}
+document.querySelector('#requestsDialogClose').onclick = function() {
+  requestsDialog.close();
+}
