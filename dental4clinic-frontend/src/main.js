497dbecb650aa6642bd94b00ad603588b88770dd
Dark Racer <schtscherbakov@gmail.com>
2023-12-24 23:34:49 +0300

    Add main page on vue

--- dental4clinic-frontend/src/main.js
+++ dental4clinic-frontend/src/main.js
@@ -2,5 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import MainPage from "@/unauthorized/MainPage.vue";
+import TheWelcome from "@/components/TheWelcome.vue";
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+  routes: [{
+    path: '/home',
+    component: MainPage
+  },
+    {
+      path: '/test',
+      component: TheWelcome
+    }
+  ],
+  history: createWebHistory()
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
