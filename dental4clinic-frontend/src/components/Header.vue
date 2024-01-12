e3d8caf593afe534d09c0749ef4a515b6a78d3e8
Dark Racer <schtscherbakov@gmail.com>
2024-01-13 01:10:12 +0300

    Add payments page for director

--- dental4clinic-frontend/src/components/Header.vue
+++ dental4clinic-frontend/src/components/Header.vue
@@ -55,7 +55,7 @@ export default {
       <!--DIRECTOR-->
       <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/record/list' });">График записей</p>
       <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/requests' });">Заявки</p>
-      <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/director/payments' });">Оплаты</p>
+      <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/payments' });">Оплаты</p>
       <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/director/prices' });">Цены</p>
       <p class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/director/doctors' });">Врачи</p>
       <div class="header-item" v-if="user.role === 'DIRECTOR'" @click="$router.push({ path: '/director/admins' });">Администраторы</div>
--- dental4clinic-frontend/src/pages/Payments.vue
+++ dental4clinic-frontend/src/pages/Payments.vue
@@ -1,12 +1,19 @@
 <script>
 import {changeClassRows} from "@/pages/js/core/table.js";
 import {get, post} from "@/pages/js/core/rest.js";
+import {useCookies} from "@vueuse/integrations/useCookies";
 
 export default {
   data() {
     return {
       paymentsTableValue: [],
       selectedIndex: -1
+    }
+  },
+  setup() {
+    const cookies = useCookies(['user_id', 'role', 'access_token'])
+    return {
+      cookies,
     }
   },
   created() {
@@ -17,6 +24,22 @@ export default {
       },
       { immediate: true }
     )
+  },
+  mounted() {
+    if (this.user.role === 'DIRECTOR') {
+      this.$refs.buttonsBlock.style.columnGap = '5%'
+    } else {
+      this.$refs.buttonsBlock.style.columnGap = '40%'
+    }
+  },
+  computed: {
+    user: function () {
+      return {
+        id: this.cookies.get("user_id"),
+        role: this.cookies.get("role"),
+        token: this.cookies.get("access_token")
+      }
+    }
   },
   methods: {
     getPayments: function () {
@@ -56,15 +79,34 @@ export default {
         }
         return
       }
-      this.selectedIndex = index
 
-      changeClassRows(this.$refs.tableRow[index].children, "cell-payments", "cell-payments-selected")
+      if (this.selectedIndex < this.paymentsTableValue.length) {
+        this.selectedIndex = index
+        changeClassRows(this.$refs.tableRow[index].children, "cell-payments", "cell-payments-selected")
+      }
+
       for(let i = 0; i < 7; i++) {
         if (index !== i) {
           changeClassRows(this.$refs.tableRow[i].children, "cell-payments-selected", "cell-payments")
         }
       }
     },
+    openDeleteDialog: function () {
+      if (this.selectedIndex > -1) {
+        this.$refs.deleteDialog.style.display = 'flex'
+        this.$refs.deleteDialog.show()
+      }
+    },
+    closeDeleteDialog: function () {
+      this.$refs.deleteDialog.style.display = null
+      this.$refs.deleteDialog.close()
+    },
+    deletePayments: function () {
+      post('payments/delete', this.getSelectedPayment()).then((data) => {
+        this.paymentsTableValue = data
+        this.closeDeleteDialog()
+      }).catch((error) => console.error(error))
+    }
   }
 }
 </script>
@@ -117,15 +159,31 @@ export default {
         </tr>
         </tbody>
       </table>
-      <div class="buttons">
+      <div class="buttons" ref="buttonsBlock">
         <div class="paid" @click="paid">Оплачено</div>
         <div class="no-paid" @click="noPaid">Не оплачено</div>
+        <div class="delete-payment" @click="openDeleteDialog" v-if="user.role === 'DIRECTOR'">Удалить</div>
       </div>
     </div>
+    <dialog class="delete-dialog-blackout" ref="deleteDialog">
+      <div class="delete-dialog-content">
+        <div class="delete-dialog-title">
+          <div class="delete-dialog-title-text">Удаление оплаты</div>
+        </div>
+        <div class="delete-dialog-body-content">
+          <div class="delete-dialog-content-description">Вы уверены что хотите удалить оплату? Это действие отменить невозможно</div>
+        </div>
+        <div class="delete-dialog-footer">
+          <div class="delete-dialog-footer-button" @click="deletePayments">Удалить</div>
+          <div class="delete-dialog-footer-button" @click="closeDeleteDialog">Отмена</div>
+        </div>
+      </div>
+    </dialog>
   </main>
 </template>
 
 <style scoped>
 @import "../assets/styles/admin/payments.scss";
+@import "../assets/styles/director/payments.scss";
 @import "../../src/main.scss";
 </style>
