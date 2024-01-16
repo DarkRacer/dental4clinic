e893394ab7dcd35b73059fb390cd36bdc6c27e78
Николай Шведов <52162935+NikolayShvedov@users.noreply.github.com>
2024-01-16 20:50:45 +0300

    Connect database and create apis

--- dental4clinic-backend/src/controllers/service/service.controller.ts
+++ dental4clinic-backend/src/controllers/service/service.controller.ts
@@ -1,2 +1,15 @@
+import { Request, Response } from "express";
+
 export class ServiceController {
+    public getDoctorService = async (req: Request, res: Response) => {
+    };
+
+    public getAllService = async (req: Request, res: Response) => {
+    };
+
+    public deleteDoctorService = async (req: Request, res: Response) => {
+    };
+
+    public addDoctorService = async (req: Request, res: Response) => {
+    };
 }
